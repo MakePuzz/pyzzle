9e0d0840f6b680ce79503507a3cbe7aa1902dafd
tsukada-cs <tsukada.cs@gmail.com>
2020-06-09 04:41:19 +0900

    Fortran導入の準備

--- example/fmain.py
+++ example/fmain.py
@@ -35,9 +35,44 @@ obj_func = [
     "max_connected_empties",
     "difficulty"
 ]
+# In[]
+blank = "*"
+cell = np.where(puzzle.cell == "", blank, puzzle.cell)
+cell = np.array(list(map(lambda x: ord(x), cell.ravel()))).reshape(puzzle.cell.shape)
+cell = np.asfortranarray(cell.astype(np.int32))
+
+n = puzzle.plc.size
+w_len_max = max(puzzle.dic.w_len)
+
+ori_s = puzzle.plc.ori
+i_s = puzzle.plc.i
+j_s = puzzle.plc.j
+
+words = list(map(lambda k: puzzle.dic.word[k], puzzle.plc.k))
+words_int = np.full([n, w_len_max], ord(blank), dtype=np.int32)
+w_lens = np.zeros(n, dtype=np.int32, order="F")
+for i in range(n):
+    word = puzzle.dic.word[puzzle.plc.k[i]]
+    w_lens[i] = len(word)
+    words_int[i,:w_lens[i]] = list(map(ord, word))
+words_int = np.asfortranarray(words_int)
+enable = np.asfortranarray(puzzle.enable.astype(np.int32))
+
+height = puzzle.height
+width = puzzle.width
+
+# In[]
+from pyzzle.Puzzle import add_to_limit
+is_used = add_to_limit(height, width, n, w_len_max, ori_s, i_s, j_s, words_int, w_lens, cell, enable)
+
+# In[]
+ocell = np.array(list(map(lambda x: chr(x), cell.ravel()))).reshape(puzzle.cell.shape)
+ocell = np.where(ocell == blank, "", ocell)
+print(ocell)
 
 
 # In[]
+"""
 puzzle.first_solve()
 
 # In[]
@@ -61,3 +96,4 @@ plt.savefig(f"fig/log_{puzzle.epoch}ep.png")
 puzzle.show()
 
 # %%
+"""
---
+++ pyzzle/Puzzle/__init__.py
@@ -0,0 +1,2 @@
+from pyzzle.Puzzle import Puzzle
+from pyzzle.Puzzle.add_to_limit import add_to_limit

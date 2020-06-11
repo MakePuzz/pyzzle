48a319fae900ab7582317cd5acfc864e1339ed48
tsukada-cs <tsukada.cs@gmail.com>
2020-06-11 17:44:46 +0900

    mask_maker.pyを修正

--- scripts/mask_maker.py
+++ scripts/mask_maker.py
@@ -20,14 +20,14 @@ gray_img = Image.open(path).convert("L")
 gray_img = np.array(gray_img.resize((int(gray_img.width*rate), int(gray_img.height*rate))))
 
 thresh = np.mean(gray_img)
-gray_01 = np.where(gray_img>=thresh, 1, 0).reshape(gray_img.shape).astype("int")
+gray_01 = np.where(gray_img>=thresh, 0, 1).reshape(gray_img.shape).astype("int")
 
 gray_str = gray_01.astype("str")
 for i in range(gray_str.shape[0]):
     gray_str[i,0] = "["+gray_str[i,0]
     gray_str[i,-1] = gray_str[i,-1]+"],"
 gray_str[0,0] = "np.array([["+gray_str[0,0][1]
-gray_str[-1,-1] = gray_str[-1,-1][0]+"]])"
+gray_str[-1,-1] = gray_str[-1,-1][0]+"]], dtype=bool)"
 
 np.savetxt(oname, gray_str, delimiter=",", fmt="%s")
 print("Successfully exported mask as '"+oname+"'. shape:"+str(gray_str.shape))

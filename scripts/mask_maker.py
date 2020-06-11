678f6309e12a5889c2593c35fb163bae81a242ab
tsukada-cs <tsukada.cs@gmail.com>
2020-06-11 17:35:36 +0900

    マスク自動生成スクリプトを追加。pillowのインストールが前提となる。
    マスク作成手順：
    1. 画像をグレースケール化
    2. 画像を二値化。この際，閾値は画像の平均輝度値
    3. Mask.pyにコピペできる形に整形してテキスト出力

---
+++ scripts/mask_maker.py
@@ -0,0 +1,33 @@
+import argparse
+
+import numpy as np
+from PIL import Image
+
+parser = argparse.ArgumentParser(description="Export mask array from input image")
+parser.add_argument("img", type=str,
+                    help="path to image")
+parser.add_argument("-r", "--rate", type=float, default=1.0,
+                    help="scale rate, default=1.0")
+parser.add_argument("-o", "--output", type=str, default="mask.txt",
+                    help="name of output file, default='mask.txt'")
+args = parser.parse_args()
+
+path = args.img
+rate = args.rate
+oname = args.output
+
+gray_img = Image.open(path).convert("L")
+gray_img = np.array(gray_img.resize((int(gray_img.width*rate), int(gray_img.height*rate))))
+
+thresh = np.mean(gray_img)
+gray_01 = np.where(gray_img>=thresh, 1, 0).reshape(gray_img.shape).astype("int")
+
+gray_str = gray_01.astype("str")
+for i in range(gray_str.shape[0]):
+    gray_str[i,0] = "["+gray_str[i,0]
+    gray_str[i,-1] = gray_str[i,-1]+"],"
+gray_str[0,0] = "np.array([["+gray_str[0,0][1]
+gray_str[-1,-1] = gray_str[-1,-1][0]+"]])"
+
+np.savetxt(oname, gray_str, delimiter=",", fmt="%s")
+print("Successfully exported mask as '"+oname+"'. shape:"+str(gray_str.shape))

65ca374732188fdfdef4a81a5e1fa7dc63128057
tsukada-cs <tsukada.cs@gmail.com>
2020-06-15 04:10:12 +0900

    add_to_limit.f90 の致命的なバグを修正

--- pyzzle/Puzzle/add_to_limit.f90
+++ pyzzle/Puzzle/add_to_limit.f90
@@ -1,128 +1,9 @@
-! program main
-!     implicit none
-!     integer, parameter :: width=5, height=5
-!     integer :: puzzle(width, height)
-!     integer :: enable(width, height)
-!     integer, parameter :: n = 6
-!     integer ::words(n)
-!     integer :: is(n), js(n), oris(n)
-!     integer :: i, j, w_len_max
-!     integer :: used_index(n)
-   
-!     puzzle(1, 1:5) = (/"H", "O", "G", "E", blank/)
-!     ! puzzle(1, 1:5) = (/blank, blank, blank, blank, blank/)
-!     puzzle(2, 1:5) = (/blank, blank, blank, blank, blank/)
-!     puzzle(3, 1:5) = (/blank, blank, blank, blank, blank/)
-!     puzzle(4, 1:5) = (/blank, blank, blank, blank, blank/)
-!     puzzle(5, 1:5) = (/blank, blank, blank, blank, blank/)
-
-!     ! This means a puzzle below:
-!     ! H * * * *
-!     ! O * * * *
-!     ! G * * * *
-!     ! E * * * *
-!     ! * * * * *
-!     enable(1, 1:5) = (/1, 1, 1, 1, 0/)
-!     enable(2, 1:5) = (/1, 1, 1, 1, 1/)
-!     enable(3, 1:5) = (/1, 1, 1, 1, 1/)
-!     enable(4, 1:5) = (/1, 1, 1, 1, 1/)
-!     enable(5, 1:5) = (/1, 1, 1, 1, 1/)
-
-!     oris(1)=0
-!     js(1)=4
-!     is(1)=1
-!     words(1)='EEE'
-
-!     ! This means a word below:
-!     ! * * * E *
-!     ! * * * E *
-!     ! * * * E *
-!     ! * * * * *
-!     ! * * * * *
-
-!     oris(2)=1
-!     js(2)=1
-!     is(2)=3
-!     words(2)='GAME'
-
-!     ! This means a word below:
-!     ! * * * * *
-!     ! * * * * *
-!     ! G A M E *
-!     ! * * * * *
-!     ! * * * * *
-
-!     oris(3)=0
-!     js(3)=3
-!     is(3)=3
-!     words(3)='MAD'
-
-!     ! This means a word below:
-!     ! * * * * *
-!     ! * * * * *
-!     ! * * M * *
-!     ! * * A * *
-!     ! * * D * *
-
-!     oris(4)=0
-!     js(4)=4
-!     is(4)=1
-!     words(4)='AAEA'
-
-!     ! This means a word below:
-!     ! * * * A *
-!     ! * * * A *
-!     ! * * * E *
-!     ! * * * A *
-!     ! * * * * *
-
-!     oris(5)=1
-!     js(5)=1
-!     is(5)=1
-!     words(5)='HAGE'
-
-!     ! This means a word below:
-!     ! H A G E *
-!     ! * * * * *
-!     ! * * * * *
-!     ! * * * * *
-!     ! * * * * *
-
-!     oris(6)=1
-!     js(6)=4
-!     is(6)=2
-!     words(6)='EQ'
-
-!     ! This means a word below:
-!     ! * * * * *
-!     ! * * * E Q
-!     ! * * * * *
-!     ! * * * * *
-!     ! * * * * *
-
-!     w_len_max = 4
-
-!     call add_to_limit(puzzle, enable, 5, 5, n, oris, is, js, words, w_len_max, used_index)
-
-!     ! write out
-!     do i = 1, height
-!         write(*,*) puzzle(1:width, i)
-!     end do
-!     do i = 1, height
-!         write(*,*) enable(1:width, i)
-!     end do
-
-
-! end program main
-
-
 subroutine add_to_limit(height, width, n, w_len_max, blank, oris, is, js, ks, words, w_lens, puzzle, enable, used_index)
     implicit none
     integer, intent(in) :: height, width
     integer, intent(in) :: n, w_len_max, blank
-    integer, dimension(:), intent(in) :: oris, is, js, ks
+    integer, dimension(:), intent(in) :: oris, is, js, ks, w_lens
     integer, dimension(:,:), intent(in) :: words
-    integer, dimension(:), intent(in) :: w_lens
     integer, dimension(:,:), intent(inout) :: puzzle
     integer, dimension(:,:), intent(inout) :: enable
     integer, dimension(n), intent(out) :: used_index
@@ -133,26 +14,27 @@ subroutine add_to_limit(height, width, n, w_len_max, blank, oris, is, js, ks, wo
     logical :: placed
 
     logical :: is_already_used
-    integer :: c, place_count!
+    integer :: c, place_count
+
     place_count = 0
     used_index = -1
 
-
     ! do i = 1, height
-    !     write(*,*) puzzle(1:width, i)
+    !     write(*,*) puzzle(i, 1:width)
     ! end do
 
     do while (.true.)
         placed = .false.
         do a = 1, n
             w_len = w_lens(a)
-            word = words(a, 1:w_len)
+            word = 0
+            word(1:w_len) = words(a, 1:w_len)
             i = is(a)
             j = js(a)
             k = ks(a)
             ori = oris(a)
-
             is_already_used = .false.
+
             if (place_count >= 1) then
                 do c = 1, place_count
                     if (k == ks(used_index(c))) then
@@ -185,7 +67,7 @@ subroutine add_to_limit(height, width, n, w_len_max, blank, oris, is, js, ks, wo
                 if (is(a)-1 >= 1) then
                     enable(is(a)-1, js(a)) = 0
                 end if
-                if (is(a)+w_len <= width) then
+                if (is(a)+w_len <= height) then
                     enable(is(a)+w_len, js(a)) = 0
                 end if
             end if
@@ -197,7 +79,7 @@ subroutine add_to_limit(height, width, n, w_len_max, blank, oris, is, js, ks, wo
                 if (js(a)-1 >= 1) then
                     enable(is(a), js(a)-1) = 0
                 end if
-                if (js(a)+w_len <= height) then
+                if (js(a)+w_len <= width) then
                     enable(is(a), js(a)+w_len) = 0
                 end if
             end if
@@ -256,37 +138,29 @@ integer function is_placeable(puzzle, enable, height, width, ori, i, j, word, w_
     ce = 0
     if (ori == 0) then ! vertical
         !First character above
-        if (i == 1) then
+        if (i <= 1) then
             cf = 1
-        else
-            if (puzzle(i-1, j) == blank) then
-                cf = 1
-            end if
+        else if (puzzle(i-1, j) == blank) then
+            cf = 1
         end if
         !Below the last letter
-        if ((i+w_len-1) == height) then
+        if (i+w_len-1 >= height) then
             ce = 1
-        else
-            if (puzzle(i+w_len, j) == blank) then
-                ce = 1
-            end if
+        else if (puzzle(i+w_len, j) == blank) then
+            ce = 1
         end if
     else if (ori == 1) then ! lateral
         !First character left
         if (j == 1) then
             cf = 1
-        else
-            if (puzzle(i, j-1) == blank) then
-                cf = 1
-            end if
+        else if (puzzle(i, j-1) == blank) then
+            cf = 1
         end if
         !Last character right
-        if ((j+w_len-1) == width) then
+        if (j+w_len-1 == width) then
             ce = 1
-        else
-            if (puzzle(i, j+w_len) == blank) then
-                ce = 1
-            end if
+        else if (puzzle(i, j+w_len) == blank) then
+            ce = 1
         end if
     end if
     if (cf == 0 .or. ce == 0) then

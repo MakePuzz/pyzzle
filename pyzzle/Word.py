d39f0e396f4616d191dac911129bbdaf51e90e0a
tsukada-cs <tsukada.cs@gmail.com>
2020-06-22 22:40:48 +0900

    Wordクラスを修正

--- pyzzle/Word.py
+++ pyzzle/Word.py
@@ -1,7 +1,7 @@
 class Word(str):
-    """wc value and string flags"""
+    """Word with weight"""
     def __new__(cls, content, *args, **kwargs):
         return super(Word, cls).__new__(cls, content)
     
-    def __init__(self, content, weight):
+    def __init__(self, content, weight=0):
         self.weight = weight

f762abb0b5def152ad196ccbe798a17c5c317be5
tsukada-cs <tsukada.cs@gmail.com>
2020-11-21 23:59:05 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,50 +9,41 @@ from numpy.distutils.core import setup, Extension
 
 
 try:
-    with open('README.md') as f:
+    with open("README.md") as f:
         readme = f.read()
 except IOError:
-    readme = ''
+    readme = ""
 
 def _requires_from_file(filename):
     return open(filename).read().splitlines()
 
-# version
-here = os.path.dirname(os.path.abspath(__file__))
-version = next((line.split('=')[1].strip().replace("'", '')
-                for line in open(os.path.join(here,
-                                              'pyzzle',
-                                              '__init__.py'))
-                if line.startswith('__version__ = ')),
-               '0.0.dev0')
-
 extensions = []
 extensions.append(
-    Extension(name='pyzzle.Puzzle.add_to_limit',
-              sources=['pyzzle/Puzzle/add_to_limit.f90'])) #f2py_options=["--opt='-O3'"]
+    Extension(name="pyzzle.Puzzle.add_to_limit",
+              sources=["pyzzle/Puzzle/add_to_limit.f90"])) #f2py_options=["--opt='-O3'"]
 
 setup(
     name="pyzzle",
-    version=version,
-    url='https://github.com/puzzle-japan/pyzzle',
-    author='The puzzle-japan Team',
-    author_email='puzzle.hokkaido@gmail.com',
-    maintainer='tsukada-cs and Saikoro2007',
-    maintainer_email='puzzle.hokkaido@gmail.com',
-    description='A Python library to automatically generate intelligent and beautifull puzzles',
+    version="0.0.1",
+    url="https://github.com/MakePuzz/pyzzle",
+    author="The MakePuzz team",
+    author_email="puzzle.hokkaido@gmail.com",
+    maintainer="The MakePuzz team",
+    maintainer_email="puzzle.hokkaido@gmail.com",
+    description="A Python library to automatically generate intelligent and beautifull puzzles",
     long_description=readme,
     packages=find_packages(),
     ext_modules=extensions,
-    install_requires=_requires_from_file('requirements.txt'),
+    install_requires=_requires_from_file("requirements.txt"),
     license="MIT",
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
     ],
     entry_points="""
       # -*- Entry points: -*-

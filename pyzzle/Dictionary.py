d54496f7d27582190086fd8c8c69be51a686e736
tsukada-cs <tsukada.cs@gmail.com>
2020-11-30 15:38:42 +0900

    rstrip()を削除

--- pyzzle/Dictionary.py
+++ pyzzle/Dictionary.py
@@ -137,7 +137,7 @@ class Dictionary:
     def read(self, dict_specifier):
         with open(dict_specifier, 'r', encoding='utf-8-sig') as f:
             data = f.readlines()
-        data = [l.rstrip() for l in data if l != os.linesep]
+        data = [l for l in data if l != os.linesep]
         # Remove new_line_code
         def removed_new_line_code(word):
             line = word.rstrip(os.linesep).split(" ")

5a914b25069e057922c24a2982cf723f0161f33d
tsukada-cs <tsukada.cs@gmail.com>
2021-01-28 09:37:21 +0900

    refactoring: main.py

--- example/main.py
+++ example/main.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 # In[]
-import sys
 from pathlib import PurePath
 
 import numpy as np
@@ -9,7 +8,6 @@ import matplotlib.pyplot as plt
 from pyzzle import Puzzle, Dictionary, Mask, Gravity
 from pyzzle import utils
 
-# Set parameters
 width = 15
 height = 15
 mask = None # Mask.donut_s # 不要ならNone
@@ -17,32 +15,21 @@ gravity = None # 不要ならNone
 dic = Dictionary("../../dictionaries/pokemon.txt")
 name = "Pyzzle"
 epoch = 3
-
 seed = 0
-np.random.seed(seed=seed)
-# In[]
-# Make instances
-puzzle = Puzzle(width=width, height=height, mask=mask, name=name) #, gravity=gravity
-
-# In[]
-puzzle.import_dict(dic)
-
-# In[]
 obj_func = [
     "uniqueness", "weight", "nwords", "area_rect", "ease_r"
 ]
-
+# In[]
+puzzle = Puzzle(width=width, height=height, mask=mask, name=name, seed=seed) #, gravity=gravity
+puzzle.import_dict(dic)
 # In[]
 utils.debug_on()
 puzzle = puzzle.solve(epoch=epoch, time_limit=None, n=1, optimizer="multi_start", of=obj_func, show=False, use_f=True)
 utils.logging_off()
-
-# In[]
-# puzzle.solve(epoch=300, optimizer="local_search", of=obj_func, show=False, use_f=True)
 # In[]
 puzzle.show()
-print(f"component should be 1: {puzzle.component}")
-print(f"unique solution: {puzzle.is_unique}")
+print(f"Component: {puzzle.component==1}")
+print(f"Uniqueness: {puzzle.is_unique}")
 
 # In[]
 base_dir = str(PurePath(__file__).parent)
@@ -62,13 +49,11 @@ if width == height == 15:
 # In[]
 puzzle.show_log()
 plt.savefig(f"{base_dir}/fig/{oname}_log.png")
-
 # %%
 # from pyzzle import PyzzleAPI
 # api = PyzzleAPI()
 # a = api.get_all_puzzles()
 # a.text
-
 
 # # %%
 # from pyzzle import Puzzle
@@ -81,5 +66,3 @@ plt.savefig(f"{base_dir}/fig/{oname}_log.png")
 # from pyzzle import Puzzle, utils
 # puzzle = Puzzle.from_json(f"json/ファイル名.json")
 # utils.export_image(puzzle.cell, puzzle.uwords[puzzle.uwords!=""], title="テーマ：テスト", oname=f"fig/twitter_test.png", answer=True)
-# %%
-# %%

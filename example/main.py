10bb87cff891d92d0e3199d694adb73b23d1f922
YuyaMatsu <kraimdeenr01241@gmail.com>
2021-04-02 22:14:53 +0900

    update utils and main.py for export_image

--- example/main.py
+++ example/main.py
@@ -47,8 +47,15 @@ if width == height == 15:
         title = f"Theme：{name}"
     else:
         title = f"テーマ：{name}"
-    utils.export_image(puzzle.cell, puzzle.uwords[puzzle.uwords!=""], title=title, oname=f"{base_dir}/fig/twitter_answer_{oname}.png", answer=True)
+
+    height = 7
+    width = 6
+    plt.rcParams.update({"figure.subplot.left": 0, "figure.subplot.bottom": 0, "figure.subplot.right": 1, "figure.subplot.top": 1})
+    fig, [axl, axr] = plt.subplots(1, 2, figsize=(9*height/7.5+width, height), gridspec_kw=dict(width_ratios=[9*height/7.5,width], wspace=-0.08))
+    utils.export_image(axl, axr, puzzle.cell, puzzle.uwords[puzzle.uwords!=""], title=title, oname=f"{base_dir}/fig/twitter_answer_{oname}.png", answer=True)
 # puzzle.to_pickle(f"pickle/{oname}.pickle")
+
+
 
 # In[]
 puzzle.show_log()

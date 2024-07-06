0e3105b565a2411bf1de4d1760d6a71372997aff
tsukada-cs <tsukada.cs@gmail.com>
2024-07-05 21:58:43 -0600

    fix main_cli again

--- example/main_cli.py
+++ example/main_cli.py
@@ -12,35 +12,29 @@ Crossword Local Search by command line
  7. 重みを考慮するかどうか（-wまたは--weightオプションでフラグとして指定. デフォルトはFalse）
  8. 出力ファイル名（-oまたは--outputオプションで指定. デフォルトは{name}.png）
 実行例：
-python main_cli.py ../src/pyzzle/dict/animals.txt 6 6 -s 1 -e 5 --use_f
+python example/main_cli.py src/pyzzle/dict/animals.txt 6 6 -s 1 -e 5 --use_f --name 動物のなまえ
 """
 # In[]
 import os
 import argparse
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 import pyzzle
+
+basedir = os.path.dirname(os.path.abspath(__file__))
 # In[]
 parser = argparse.ArgumentParser(description="make a puzzle with given parameters")
-parser.add_argument("dict_path", type=str,
-                    help="file path of a dictionary")
-parser.add_argument("width", type=int,
-                    help="width of the puzzle")
-parser.add_argument("height", type=int,
-                    help="height of the puzzle")
-parser.add_argument("-s", "--seed", type=int, default=66666,
-                    help="random seed value, default=66666")
-parser.add_argument("-e", "--epoch", type=int, default=10,
-                    help="epoch number of local search, default=10")
-parser.add_argument("-n", "--name", type=str,
-                    help="name of the puzzle")
-parser.add_argument("-w", "--weight", action="store_true",
-                    help="flag of consider the weight, default=False")
-parser.add_argument("-o", "--output", type=str,
-                    help="name of the output image file")
-parser.add_argument("-f", "--use_f", action="store_true",
-                    help="use Fortran function in local search")
+parser.add_argument("dict_path", type=str, help="file path of a dictionary")
+parser.add_argument("width", type=int, help="width of the puzzle")
+parser.add_argument("height", type=int, help="height of the puzzle")
+parser.add_argument("-s", "--seed", type=int, default=66666, help="random seed value, default=66666")
+parser.add_argument("-e", "--epoch", type=int, default=10, help="epoch number of local search, default=10")
+parser.add_argument("-n", "--name", type=str, help="name of the puzzle")
+parser.add_argument("-w", "--weight", action="store_true", help="flag of consider the weight, default=False")
+parser.add_argument("-o", "--output", type=str, help="name of the output image file")
+parser.add_argument("-f", "--use_f", action="store_true", help="use Fortran utility in local search")
 args = parser.parse_args()
 
 # settings
@@ -63,7 +57,7 @@ dic = pyzzle.Dictionary(dict_path)
 
 if name is None:
     name = os.path.basename(dict_path).split(".")[0]
-puzzle.puzzle_name = name
+puzzle.name = name
 if output is None:
     output = name + ".png"
 if with_weight:
@@ -74,7 +68,7 @@ else:
 puzzle.import_dict(dic)
 optimizer = pyzzle.Optimizer.LocalSearch(show=False, shrink=False, use_f=args.use_f)
 puzzle = puzzle.solve(epoch=epoch, optimizer=optimizer, of=obj_func)
-puzzle.export_json(f"json/{puzzle.name}_w{width}_h{height}_r{seed}.json")
+puzzle.export_json(f"{basedir}/json/{puzzle.name}_w{width}_h{height}_r{seed}.json")
 
 def is_halfwidth(char):
     return len(char.encode('utf-8')) <= 1
@@ -83,17 +77,24 @@ if is_halfwidth(dic[0]["word"][0]): # 半角ならTrue
     halfspace = True
 else:
     halfspace = False
-
-if width == height == 15:
-    if halfspace: # 英数字ならTrue
-        title = f"Theme：{name}"
-    else:
-        title = f"テーマ：{name}"
-    oname_problem = f"fig/twitter_problem_{puzzle.name}_w{width}_h{height}_r{seed}.png"
-    oname_answer = f"fig/twitter_answer_{puzzle.name}_w{width}_h{height}_r{seed}.png"
-    pyzzle.utils.export_image(puzzle.cell, puzzle.uwords[puzzle.uwords!=""], title=title, oname=oname_problem, dpi=144, answer=False)
-    pyzzle.utils.export_image(puzzle.cell, puzzle.uwords[puzzle.uwords!=""], title=title, oname=oname_answer, dpi=144, answer=True)
-    print(f"Saved as {oname_answer} and {oname_problem}")
-
-
 pyzzle.utils.show_2Darray(puzzle.cell, puzzle.mask, halfspace=halfspace)
+
+
+if halfspace: # 英数字ならTrue
+    title = f"Theme: {name}"
+else:
+    title = f"テーマ：{name}"
+oname_problem = f"{basedir}/fig/problem_{puzzle.name}_w{width}_h{height}_r{seed}.png"
+oname_answer = f"{basedir}/fig/answer_{puzzle.name}_w{width}_h{height}_r{seed}.png"
+
+height_inch = 7
+width_inch = 6
+fig, [axl, axr] = plt.subplots(1, 2, figsize=(9*height_inch/7.5+width_inch, height_inch), gridspec_kw=dict(width_ratios=[9*height_inch/7.5,width_inch], wspace=-0.01))
+pyzzle.utils.export_image(fig, axl, axr, puzzle.cell, puzzle.uwords[puzzle.uwords!=""], width_inch, height_inch, title=title, fontsize=18, oname=oname_answer, answer=True)
+plt.close()
+
+fig, [axl, axr] = plt.subplots(1, 2, figsize=(9*height_inch/7.5+width_inch, height_inch), gridspec_kw=dict(width_ratios=[9*height_inch/7.5,width_inch], wspace=-0.01))
+pyzzle.utils.export_image(fig, axl, axr, puzzle.cell, puzzle.uwords[puzzle.uwords!=""], width_inch, height_inch, title=title, fontsize=18, oname=oname_problem, answer=False)
+plt.close()
+
+print(f"Saved as {oname_answer} and {oname_problem}")
